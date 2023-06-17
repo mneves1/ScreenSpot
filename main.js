415d8e75b693f6d8b0510558e2455ccba1868a1d
Midiel Henriquez <Midielhg@gmail.com>
2023-06-16 21:19:11 -0400

    Adding more genre and trailer on hero section

--- main.js
+++ main.js
@@ -7,3 +7,26 @@ window.onload = function() {
     mobile_menu.classList.toggle('is-active');
   });
 };
+// Array of YouTube video IDs
+const videoIDs = ['LM2F56uK0fs', 'X9ebeNKkc08', 'cnDObXxwWy0', 'LM2F56uK0fs'];
+
+// Generate a random index
+const randomIndex = Math.floor(Math.random() * videoIDs.length);
+
+// Get the random video ID
+const randomVideoID = videoIDs[randomIndex];
+
+// Load the YouTube player
+function onYouTubePlayerAPIReady() {
+  new YT.Player('player', {
+    videoId: randomVideoID,
+    playerVars: {
+      autoplay: 1,
+      mute: 1,
+      loop: 1,
+      playlist: randomVideoID,
+      controls: 0,
+      disablekb: 1
+    }
+  });
+}
